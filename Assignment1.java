ccae3010cbf352c7543540e690d0f56a0adeb2aa
Arul-virumbi-16 <65747400+Arul-virumbi-16@users.noreply.github.com>
2020-08-15 17:20:42 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,15 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		double product1 = 10.5;
+		double product2 = 25.2;
+		double product3 = 30;
+
+		double tax = 5;
+		double total = product1 + product2 + product3;
+		double finalamt = (tax/100.0)*total;
+		System.out.println("Final Amount " + finalamt + total);
+	}
+
+}
